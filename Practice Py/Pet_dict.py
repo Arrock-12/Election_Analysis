3db75b6fb80f3c1f5fba3e35306246e6c5d8c718
Arrock-12 <101822948+Arrock-12@users.noreply.github.com>
2022-05-07 11:16:16 -0500

    Delete Practice Py directory
    File accidentally included that wasn't needed for analysis

--- Practice Py/Pet_dict.py
+++
@@ -1,9 +0,0 @@
-pet_dict = {
-"pet_name":"Lefty", 
-"Pet_age":"Old AF", 
-"Hobbies":["licking", "whining", "barking"],
-"times":{"Monday":8, "Tuesday":8, "Wednesday":7}}
-print(f'{pet_dict["pet_name"]} is {pet_dict["Pet_age"]}')
-
-
-
